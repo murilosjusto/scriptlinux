0a19b9bc53882cd0dd2cc2fc2c575c904752e999
murilosjusto <77011141+murilosjusto@users.noreply.github.com>
2023-07-10 11:43:44 -0300

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,4 +2,4 @@
 
 # Script para instalar o Google Chrome (Debian 12)
 
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/linux_signing_key.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/linux_signing_key.gpg /dev/null 2>&1 && sudo apt update && sudo apt install google-chrome-stable
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/linux_signing_key.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/linux_signing_key.gpg /dev/null 2>&1 && sudo apt update && sudo apt install -y google-chrome-stable
